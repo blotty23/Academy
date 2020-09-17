423b3a3abb425fa0ab3299caa268cccc63e2d8a1
Emma Blott <blott23@gmail.com>
2020-09-17 13:51:30 +0100

    wrote test for 3=> 'Fizz', which fails

--- 08_TDD_Practice/FizzBuzz/spec/fizzbuzz_spec.rb
+++ 08_TDD_Practice/FizzBuzz/spec/fizzbuzz_spec.rb
@@ -5,7 +5,7 @@ describe "Fizzbuzz_spec.rb" do
     end
 end
 
-describe "Fizzbuzz" do
+describe "Fizzbuzz_simple" do
     it  "given 1 ruturns 1" do
         expect(Fizzbuzz(1)).to eq("1")
     end
@@ -14,5 +14,11 @@ describe "Fizzbuzz" do
     end 
     it "given 4 returns 4" do
         expect(Fizzbuzz(4)).to eq("4")
+    end
+end
+
+describe "Fizzbuzz single multiples" do
+    it  "given 3 returns Fizz" do
+        expect(Fizzbuzz(3)).to eq("Fizz")
     end
 end
