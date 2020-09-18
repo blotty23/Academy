4f02715aeb4dfdb917602f41d4cd936d0abceed6
Emma Blott <blott23@gmail.com>
2020-09-18 14:07:28 +0100

    Refactored to recursion instead of .times loop - still passes tests

--- 08_TDD_Practice/Nth_Fibonacci/lib/nth_fibonacci.rb
+++ 08_TDD_Practice/Nth_Fibonacci/lib/nth_fibonacci.rb
@@ -2,10 +2,10 @@ def fibonacci(position)
     sequence = [0,1]
     return sequence[0] if position == 1 
     return sequence[1] if position == 2
-
-    position-2.times do
-        sequence.push(sequence[-1]+ sequence [-2])
-    end
+    fibonacci(position - 1) + fibonacci(position -2)
+    # position-2.times do
+    #     sequence.push(sequence[-1]+ sequence [-2])
+    # end
 
     
 end 
