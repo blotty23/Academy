bf306719a3808a15d7b006f4d05d98a06d04f534
Emma Blott <blott23@gmail.com>
2020-09-18 08:25:09 +0100

    added sequencing answer to lib file but not finished

--- 08_TDD_Practice/Nth_Fibonacci/lib/nth_fibonacci.rb
+++ 08_TDD_Practice/Nth_Fibonacci/lib/nth_fibonacci.rb
@@ -1,7 +1,12 @@
 def fibonacci(position)
-    return 0 if position == 1 
-    return 1 if position == 2
+    sequence = [0,1]
+    return sequence[0] if position == 1 
+    return sequence[1] if position == 2
+
+    sequence.push(sequence[-1]+ sequence [-2])
     return 0 + 1 if position == 3
     return 1 + 1 if position == 4
     return 1 + 2 if position == 5
+
+    
 end 
