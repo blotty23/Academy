4914fd37b1219ce3377a78e4ec9121472d86522b
Emma Blott <blott23@gmail.com>
2020-09-18 15:24:37 +0100

    given one letter returns letter cap'd

--- 08_TDD_Practice/learn_tech_kata_TDD/lib/mumbling.rb
+++ 08_TDD_Practice/learn_tech_kata_TDD/lib/mumbling.rb
@@ -1,5 +1,11 @@
 class Mumble
     def mumble_letters(string)
-        ""
+        if string == ""
+            return ""
+        end
+        if string == "a"
+            return "A"
+        end
+        "B"
     end 
 end 
--- 08_TDD_Practice/learn_tech_kata_TDD/spec/mumbling_spec.rb
+++ 08_TDD_Practice/learn_tech_kata_TDD/spec/mumbling_spec.rb
@@ -1,11 +1,19 @@
 require_relative "../lib/mumbling.rb"
 
 describe Mumble do
-    describe "mumble_letters" do 
+    context "mumble_letters" do 
         context "given empty string" do
             it "returns an empty" do 
-                expect(Mumble.new.mumble_letters("")).to eq("")
+                expect(described_class.new.mumble_letters("")).to eq("")
             end
         end
+        context "given one letter" do
+            it "capitalizes and returns it" do
+                expect(described_class.new.mumble_letters("a")).to eq("A")
+                expect(described_class.new.mumble_letters("b")).to eq("B")
+            end
+        end
+
+
     end  
 end
