c5bf7033332f52afaf034b3c1170ba5e975734b0
Emma Blott <blott23@gmail.com>
2020-09-20 23:48:06 +0100

    last commit before EOD friday - almost got it

--- 08_TDD_Practice/learn_tech_kata_TDD/lib/mumbling.rb
+++ 08_TDD_Practice/learn_tech_kata_TDD/lib/mumbling.rb
@@ -2,47 +2,47 @@ class Mumble
 
     def mumble_letters(string)
 
-        if string.length == 2
-            result = ""
-            result << string[0].capitalize
-            result << "-"
-            result << string[1].capitalize
-            result << string[1]
-            return result
-        end
+        chars = string.split('')
 
-        if string.length == 3
-            result = ""
-            result << string[0].capitalize
-            result << "-"
-            result << string[1].capitalize
-            result << string[1]
-            result << "-"
-            result << string[2].capitalize
-            result << string[2]
-            result << string[2]
-            return result
+        return chars.map.with_index { |char, i| (char*i).capitalize }.join('-') 
 
-        end
+        # if string.length == 2
+        #     result = string[0].capitalize
+        #     for i in 1...2
+        #         result << "-" << string[i].capitalize
+        #         result << string[i]
+        #     end
+        #     return result
+        # end
 
-        if string.length == 4
-            result = ""
-            result << string[0].capitalize
-            result << "-"
-            result << string[1].capitalize
-            result << string[1]
-            result << "-"
-            result << string[2].capitalize
-            result << string[2]
-            result << string[2]
-            result << "-"
-            result << string[3].capitalize
-            result << string[3]
-            result << string[3]
-            result << string[3]
-            return result
-        end
+        # if string.length == 3
+        #     result = string[0].capitalize
+        #     for i in 1...3
+        #         result << "-" << string[i].capitalize
+        #         result << string[i]*i
+        #     end
+        #     return result
 
-        string.capitalize
+        # end
+
+        # if string.length == 4
+        #     result = ""
+        #     result << string[0].capitalize
+        #     result << "-"
+        #     result << string[1].capitalize
+        #     result << string[1]
+        #     result << "-"
+        #     result << string[2].capitalize
+        #     result << string[2]
+        #     result << string[2]
+        #     result << "-"
+        #     result << string[3].capitalize
+        #     result << string[3]
+        #     result << string[3]
+        #     result << string[3]
+        #     return result
+        # end
+
+        # string.capitalize
     end 
 end 
