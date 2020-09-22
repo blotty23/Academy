5e6d68c3fb194250f0193bd2b015c1809bdc5a70
Emma Blott <blott23@gmail.com>
2020-09-22 19:23:24 +0100

    removed initial environment setup test

--- 08_TDD_Practice/leap_year/spec/leap_year_spec.rb
+++ 08_TDD_Practice/leap_year/spec/leap_year_spec.rb
@@ -1,10 +1,5 @@
 require_relative "../lib/leap_year.rb"
 
-describe "My Environment" do 
-    it " runs tests - true = true" do
-        expect(true).to eq(true)
-    end 
-end 
 
 describe "LeapYear" do
     it "Typical common year: returns false when given 2001" do     
