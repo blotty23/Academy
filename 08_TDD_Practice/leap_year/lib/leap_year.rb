44a043fdb4291e2ac5c8b0d0f7d968858bba7471
Emma Blott <blott23@gmail.com>
2020-09-22 19:40:37 +0100

    solved for atypical leap year

--- 08_TDD_Practice/leap_year/lib/leap_year.rb
+++ 08_TDD_Practice/leap_year/lib/leap_year.rb
@@ -1,9 +1,7 @@
 class LeapYear
 
     def is_leap_year? (year)
-        return true if year == 2000
-        return true if year == 2400
-        
+        return true if year%400 == 0
         return false if year%100 == 0
         return true if year%4 == 0
         false
