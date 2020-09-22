3059643f8ebc3430ccf64b5cb4e52b5f9c0f2a5b
Emma Blott <blott23@gmail.com>
2020-09-22 19:36:28 +0100

    solved for atypical common year by abstraction'

--- 08_TDD_Practice/leap_year/lib/leap_year.rb
+++ 08_TDD_Practice/leap_year/lib/leap_year.rb
@@ -1,7 +1,7 @@
 class LeapYear
 
     def is_leap_year? (year)
-        return false if year == 1900
+        return false if year%100 == 0
         return true if year%4 == 0
         false
     end 
