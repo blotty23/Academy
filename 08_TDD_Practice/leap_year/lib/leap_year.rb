3d010f99edbf84d617791b024bc10958703fdb5c
Emma Blott <blott23@gmail.com>
2020-09-22 19:39:26 +0100

    solved new test case for atypical leap year by sliming

--- 08_TDD_Practice/leap_year/lib/leap_year.rb
+++ 08_TDD_Practice/leap_year/lib/leap_year.rb
@@ -2,6 +2,8 @@ class LeapYear
 
     def is_leap_year? (year)
         return true if year == 2000
+        return true if year == 2400
+        
         return false if year%100 == 0
         return true if year%4 == 0
         false
