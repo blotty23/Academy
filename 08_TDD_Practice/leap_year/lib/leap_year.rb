1b2d767bbd08b912a9a9e8a03cd8cb85c7dbb014
Emma Blott <blott23@gmail.com>
2020-09-22 19:28:40 +0100

    added spec helper and new test for typical year

--- 08_TDD_Practice/leap_year/lib/leap_year.rb
+++ 08_TDD_Practice/leap_year/lib/leap_year.rb
@@ -1,6 +1,7 @@
 class LeapYear
 
     def is_leap_year? (year)
+        return true if year == 1996
         false
     end 
 
--- 08_TDD_Practice/leap_year/spec/leap_year_spec.rb
+++ 08_TDD_Practice/leap_year/spec/leap_year_spec.rb
@@ -5,7 +5,10 @@ describe "LeapYear" do
     it "Typical common year: returns false when given 2001" do     
         expect(LeapYear.new.is_leap_year?(2001)).to eq(false)
     end 
-    it "Typical leap year: returns true when given 1996" do
-        expect(LeapYear.new.is_leap_year?(1996)).to eq(true)
+    leap_years = [1996, 1992]
+    leap_years.each do |year|
+        it "Typical leap year: returns true when given #{year}" do
+            expect(LeapYear.new.is_leap_year?(year)).to eq(true)
+        end 
     end 
 end 
